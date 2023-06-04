ace5634cc82b93f191cc92bb1f00bd6cfadad3f9
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2023-06-04 11:59:35 +0200

    centering tabRows in topBar

--- app/src/main/java/com/example/myhomebudgetcompose/ui/components/TabRow.kt
+++ app/src/main/java/com/example/myhomebudgetcompose/ui/components/TabRow.kt
@@ -21,6 +21,7 @@ import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -37,6 +38,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -59,7 +61,7 @@ fun TabRow(
             .height(TabHeight)
             .fillMaxWidth()
     ) {
-        Row(Modifier.selectableGroup()) {
+        Row(Modifier.selectableGroup(), horizontalArrangement = Arrangement.Center) {
             allScreens.forEach { screen ->
                 SingleTab(
                     text = screen.route,
